03b8c886b077eab22d574f468d9937bc1293cf62
phuccoder <anhdeptraippt@gmail.com>
2025-05-29 17:44:08 +0700

    fix Chunck Error

--- src/pages/protected/Welcome.js
+++ src/pages/protected/Welcome.js
@@ -1,14 +1,12 @@
-import { useEffect, Suspense } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setPageTitle } from '../../features/common/headerSlice';
 import { Link } from 'react-router-dom';
-
-// Import TemplatePointers directly since we're not using lazy loading
 import TemplatePointers from '../../features/user/components/TemplatePointers';
 
 function InternalPage() {
     const dispatch = useDispatch();
-    const userRole = localStorage.getItem('role');
+
 
     useEffect(() => {
         dispatch(setPageTitle({ title: "Welcome" }));
@@ -17,22 +15,12 @@ function InternalPage() {
     return (
         <div className="min-h-screen flex justify-center items-start mt-6 bg-base-200">
             <div className="w-full max-w-4xl bg-base-100 shadow-xl rounded-lg p-8 flex flex-col items-center text-center">
-                {/* No need for Suspense if not using lazy loading */}
                 <TemplatePointers />
-
-                {userRole === 'MANAGER' ? (
-                    <Link to="/app/project-list">
-                        <button className="mt-4 btn btn-primary hover:scale-105 transition duration-300">
-                            Get Started 🚀
-                        </button>
-                    </Link>
-                ) : (
                     <Link to="/app/dashboard">
                         <button className="mt-4 btn btn-primary hover:scale-105 transition duration-300">
                             Get Started 🚀
                         </button>
                     </Link>
-                )}
             </div>
         </div>
     );
