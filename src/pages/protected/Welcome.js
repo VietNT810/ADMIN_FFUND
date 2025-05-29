b6622f7195dad472b570be6e3153c67129d19515
phuccoder <anhdeptraippt@gmail.com>
2025-05-29 16:46:56 +0700

    fix project completed for manager

--- src/pages/protected/Welcome.js
+++ src/pages/protected/Welcome.js
@@ -7,8 +7,6 @@ import TemplatePointers from '../../features/user/components/TemplatePointers';
 function InternalPage() {
     const dispatch = useDispatch();
 
-    const userRole = localStorage.getItem('role');
-
     useEffect(() => {
         dispatch(setPageTitle({ title: "Welcome" }));
     }, [dispatch]);
@@ -17,19 +15,11 @@ function InternalPage() {
         <div className="min-h-screen flex justify-center items-start mt-6 bg-base-200">
             <div className="w-full max-w-4xl bg-base-100 shadow-xl rounded-lg p-8 flex flex-col items-center text-center">
                 <TemplatePointers />
-                {userRole === 'MANAGER' ? (
-                    <Link to="/app/project-list">
-                        <button className="mt-4 btn btn-primary hover:scale-105 transition duration-300">
-                            Get Started 🚀
-                        </button>
-                    </Link>
-                ) : (
-                    <Link to="/app/dashboard">
-                        <button className="mt-4 btn btn-primary hover:scale-105 transition duration-300">
-                            Get Started 🚀
-                        </button>
-                    </Link>
-                )}
+                <Link to="/app/dashboard">
+                    <button className="mt-4 btn btn-primary hover:scale-105 transition duration-300">
+                        Get Started 🚀
+                    </button>
+                </Link>
             </div>
         </div>
     );
